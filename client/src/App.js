4f7abfe90e119e80279d7a492977cb1e4b68270d
PagilayaSkumbria54 <toni.pashev@bk.ru>
2023-03-12 01:38:06 +0300

    Add Navbar and Auth page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
+import NavBar from "../src/components/NavBar";
 function App() {
   return (
     <BrowserRouter>
+        <NavBar />
         <AppRouter/>
     </BrowserRouter>
   );
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import {Button, Card, Container, Form} from "react-bootstrap";
+import {NavLink, useLocation} from "react-router-dom";
+import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/consts";
 
 const Auth = () => {
+    const location = useLocation()
+    const isLogin = location.pathname === LOGIN_ROUTE
     return (
-        <div>
-            AUTH
-        </div>
+        <Container
+            className="d-flex justify-content-center align-items-center"
+            style={{height: window.innerHeight - 54}}
+        >
+            <Card style={{width: 600}} className="p-5">
+                <h2 className="m-auto">{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите email"
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите пароль"
+                    />
+                    <Form className="d-flex justify-content-between align-items-center">
+                        {isLogin ?
+                            <div>
+                                Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
+                            </div>
+                            :
+                            <div>
+                                Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
+                            </div>
+                        }
+
+                        <Button
+                            className="mt-3 align-self-end"
+                            variant={"outline-success"}>{isLogin ? 'Войдите' : 'Регистрация'}</Button>
+                    </Form>
+                </Form>
+            </Card>
+        </Container>
     );
 };
 
